dc72e4da748adb8283243ff62c17bb58befa020d
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2023-10-20 20:20:08 +0530

    Add files via upload

---
+++ Python Tutorial/pr1_firstProgram.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 00:24:42 2023
+
+@author: Soumyadip
+"""
+
+print("Hello World")
---
+++ Python Tutorial/pr2_dataType.py
@@ -0,0 +1,30 @@
+# =============================================================================
+#   DATA TYPES OF VARIABLE IN PYTHON
+# =============================================================================
+
+firstNumber = 10
+print(type(firstNumber))
+secongNumber = 10.2
+print(type(secongNumber))
+thirdNumber = 25.463
+print(type(thirdNumber))
+fourthNumber = -1.322
+print(type(fourthNumber))
+fifthNumber = -1
+print(type(fifthNumber))
+sixthNumber = 4568.235686954
+print(type(sixthNumber))
+sentence = "Hello World"
+print(type(sentence))
+number = 4 + 4.236589
+print(type(number))
+number = complex(4 + (22/7)*1j)
+print(number); print(type(number))
+check_case = True
+print(type(check_case))
+check_case = False
+print(type(check_case))
+line = "Welcome to Python Programming language\n               on SPYDER IDE editor."
+print(line)
+line = "Welcome to Python Programming language\n               \r on SPYDER IDE editor"
+print(line)
