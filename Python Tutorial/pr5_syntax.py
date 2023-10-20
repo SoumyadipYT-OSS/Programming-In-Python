ecb35bb7f9ca2cea844c5e78d6766775895cb32c
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2023-10-20 20:28:12 +0530

    upto pr6 updated

---
+++ Python Tutorial/pr5_syntax.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 20:50:56 2023
+
+@author: Soumyadip
+"""
+
+# =============================================================================
+#   SYNTAX IN PYTHON
+# =============================================================================
+
+prefix = "Sir"; firstName = "Ratan"; lastName = "Tata";
+print(prefix,firstName,lastName)
+m, n = 1, 2
+m = m+n
+print(m)
---
+++ Python Tutorial/pr6_working_with_numbers.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 21:24:48 2023
+
+@author: soumy
+"""
+
+# =============================================================================
+#   WORKING WITH NUMBERS IN PYTHON
+# =============================================================================
+print(abs(3.0))
+print(abs(-3.0))
+print("Binary value of 11 is: ",bin(11))
+print("Octal value of 11 is: ",oct(11))
+print("Hexadecimal value of 11 is: ",hex(11))
+print("Round figure: ", round(3.25))
+print("Round figure: ", round(3.48))
+print("Round figure: ", round(3.4))
+print("Round figure: ", round(3.5))
+print("Round figure: ", round(3.6))
+print("Round figure: ", round(3.50))
+print("Round figure: ", round(3.60))
+print("Round figure: ", round(3.68))
+print("Round figure: ", round(3.99))
+print(max(1, 2, 3))
+print(min(1, 2, 3))
+
+import math
+print(math.sqrt(25))
+print(round(math.sqrt(25)))
