a7553fffa2904847bf40585eab3bc9764636ff7b
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2023-10-23 22:35:08 +0530

    upto pr15 updated

---
+++ Python Tutorial/pr15_loop.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 23:41:35 2023
+
+@author: soumyadip
+
+Topic : Loop
+"""
+n = 0
+while n < 5:
+    if n==3:
+        n = n+1
+    print(n)        
+    n = n+1
+    
+    
+print("Example")
+n = 0
+while True:
+    if n == 3:
+        break
+    print(n)
+    n = n+1
